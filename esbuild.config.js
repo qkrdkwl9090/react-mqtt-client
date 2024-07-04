e7f7c20466c2370fc5a4595b9dfecdc7b203f76f
Tony <qkrdkwl9090@naver.com>
2024-07-05 00:18:46 +0900

    feat: add index.ts

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,71 +1,32 @@
 const esbuild = require('esbuild');
 const { dependencies } = require('./package.json');
-const babel = require('@babel/core');
+const fs = require('fs');
+
+if (fs.existsSync('./dist')) {
+  fs.rmSync('./dist', { recursive: true });
+}
 
 const sharedConfig = {
   entryPoints: ['src/index.ts'],
   bundle: true,
   external: Object.keys(dependencies),
   sourcemap: true,
-  loader: {
-    '.js': 'jsx',
-    '.ts': 'tsx',
-  },
+  minify: true,
 };
 
-esbuild.build({
-  ...sharedConfig,
-  outfile: 'dist/index.js',
-  format: 'cjs',
-  platform: 'node',
-  plugins: [
-    {
-      name: 'babel',
-      setup(build) {
-        build.onLoad({ filter: /\.[jt]sx?$/ }, async (args) => {
-          const source = await require('fs').promises.readFile(
-            args.path,
-            'utf8',
-          );
-          const { code } = await babel.transformAsync(source, {
-            filename: args.path,
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-          });
-          return { contents: code, loader: 'jsx' };
-        });
-      },
-    },
-  ],
-});
+esbuild
+  .build({
+    ...sharedConfig,
+    outfile: 'dist/index.js',
+    format: 'cjs',
+    platform: 'node',
+  })
+  .catch(() => process.exit(1));
 
-esbuild.build({
-  ...sharedConfig,
-  outfile: 'dist/index.esm.js',
-  format: 'esm',
-  plugins: [
-    {
-      name: 'babel',
-      setup(build) {
-        build.onLoad({ filter: /\.[jt]sx?$/ }, async (args) => {
-          const source = await require('fs').promises.readFile(
-            args.path,
-            'utf8',
-          );
-          const { code } = await babel.transformAsync(source, {
-            filename: args.path,
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-          });
-          return { contents: code, loader: 'jsx' };
-        });
-      },
-    },
-  ],
-});
+esbuild
+  .build({
+    ...sharedConfig,
+    outfile: 'dist/index.esm.js',
+    format: 'esm',
+  })
+  .catch(() => process.exit(1));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as useMQTT } from './useMQTT';
+export { default as useSubscription } from './useSubscription';
+export { default as useSubscribe } from './useSubscribe';
+export { default as usePublish } from './usePublish';
+export { default as useMQTTConnectionStatus } from './useMQTTConnectionStatus';
+export { default as useMQTTMessageHandler } from './useMQTTMessageHandler';
